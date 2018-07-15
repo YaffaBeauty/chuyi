fe3ce2e5eeebf3652971e26a457081226b2f9bed
YaffaBeauty <401638860@qq.com>
2018-07-16 00:22:53 +0800

    数组分成和相等的两部分

--- leetcode/7W2.py
+++ leetcode/7W2.py
@@ -4,10 +4,10 @@
 """将数组分成两部分，两部分中的和相等"""
 
 
-class Solution():
+class Solution(object):
     def canPartition(self, nums):
         if nums == []:
-            return True
+            return False
         if sum(nums) % 2 == 1:
             return False
         half_sum = int(sum(nums) / 2)
@@ -20,6 +20,7 @@ class Solution():
             j = half_sum
             while j >= i:
                 res[j] = res[j] or res[j - i]
+
                 j = j - 1
         return res[half_sum]
 
@@ -28,4 +29,4 @@ if __name__ == '__main__':
     a = [5, 10, 4, 25, 23, 21]
     b = [1, 2, 3, 5]
     c = [1, 5, 11, 5]
-    print(Solution().canPartition(c))
+    print(Solution().canPartition(a))
